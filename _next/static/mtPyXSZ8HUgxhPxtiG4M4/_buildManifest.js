17c1e2b76d16b74c2e5684a1ede8cdbc7b2d307b
jacklenzotti <jacklenzotti@gmail.com>
2020-06-19 20:22:34 -0400

    Updates

---
+++ _next/static/mtPyXSZ8HUgxhPxtiG4M4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F836ce6ee2e5e84f8ae2a42fb6c8e4a3e5c55ab93.d95b83363bd0a458957f.js","static\u002Fcss\u002F55cbf9c4aaf9b0dd33f6.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
