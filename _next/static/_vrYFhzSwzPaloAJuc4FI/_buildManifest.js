b672d9f38154a2bed5828ea286e3920b42766c19
jacklenzotti <jacklenzotti@gmail.com>
2020-06-19 20:10:11 -0400

    Updates

---
+++ _next/static/_vrYFhzSwzPaloAJuc4FI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F71dcda71f9b57d6fa9d809eb0a62bb4e8d0240d1.52f3d7cd7e6f7b7d586d.js","static\u002Fcss\u002F55cbf9c4aaf9b0dd33f6.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/sJGk-agjAA59CjwypDqX6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F8f5108a33a5d5f4eb7b9dc938c9674c53731a570.d95b83363bd0a458957f.js","static\u002Fcss\u002F55cbf9c4aaf9b0dd33f6.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
