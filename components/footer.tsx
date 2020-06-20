5485ba03609b214b279c4f9489b8f43ec7112939
Jack <lenzottij24@gmail.com>
2020-06-20 14:56:31 -0400

    adding "built with" to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,16 +13,21 @@ import {
 
 export default function Footer() {
     return (
-      <div className={styles.footerContainer}>
-            <a target="_blank" href="https://www.linkedin.com/in/jack-l-641496106/" className={`${styles.social} fa-2x`}>
-                <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a target="_blank" href="https://github.com/jacklenzotti" className={`${styles.social} fa-2x`}>
-                <FontAwesomeIcon icon={faGithubSquare} />
-            </a>
-            <a target="_blank" href="https://open.spotify.com/user/jack_benett_?si=wQLNF0-cRm-88aJNup-mGA" className={`${styles.social} fa-2x`}>
-                <FontAwesomeIcon icon={faSpotify} />
-            </a>
+      <div className={`${styles.footerContainer} ${utilStyles.extraLightText}`}>
+        <div>
+          <a target="_blank" href="https://www.linkedin.com/in/jack-l-641496106/" className={`${styles.social} fa-2x`}>
+              <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a target="_blank" href="https://github.com/jacklenzotti" className={`${styles.social} fa-2x`}>
+              <FontAwesomeIcon icon={faGithubSquare} />
+          </a>
+          <a target="_blank" href="https://open.spotify.com/user/jack_benett_?si=wQLNF0-cRm-88aJNup-mGA" className={`${styles.social} fa-2x`}>
+              <FontAwesomeIcon icon={faSpotify} />
+          </a>
+        </div>
+        <span className={`${styles.builtWith}`}>
+          Built with <span className={styles.love}>❤</span> using NextJS, React & TypeScript
+        </span>
       </div>
     )
   }
