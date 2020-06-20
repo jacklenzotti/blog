d4de27c75e9062fe468c5733bdfbf364a9cdd662
Jack <lenzottij24@gmail.com>
2020-06-19 20:32:13 -0400

    change repo name

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/personal-blog/' : '',
+  assetPrefix: !debug ? '/blog/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
