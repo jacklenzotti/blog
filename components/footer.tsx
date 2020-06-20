4eac002a04601a0c48fbf4593cd77eaf81c8db73
Jack <lenzottij24@gmail.com>
2020-06-20 14:31:53 -0400

    add target=_blank to social media links

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,13 +14,13 @@ import {
 export default function Footer() {
     return (
       <div className={styles.footerContainer}>
-            <a href="https://www.linkedin.com/in/jack-l-641496106/" className={`${styles.social} fa-2x`}>
+            <a target="_blank" href="https://www.linkedin.com/in/jack-l-641496106/" className={`${styles.social} fa-2x`}>
                 <FontAwesomeIcon icon={faLinkedin} />
             </a>
-            <a href="https://github.com/jacklenzotti" className={`${styles.social} fa-2x`}>
+            <a target="_blank" href="https://github.com/jacklenzotti" className={`${styles.social} fa-2x`}>
                 <FontAwesomeIcon icon={faGithubSquare} />
             </a>
-            <a href="https://open.spotify.com/user/jack_benett_?si=wQLNF0-cRm-88aJNup-mGA" className={`${styles.social} fa-2x`}>
+            <a target="_blank" href="https://open.spotify.com/user/jack_benett_?si=wQLNF0-cRm-88aJNup-mGA" className={`${styles.social} fa-2x`}>
                 <FontAwesomeIcon icon={faSpotify} />
             </a>
       </div>
