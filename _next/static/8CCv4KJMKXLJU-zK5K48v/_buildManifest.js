e18df6fdd4a9b72bde18ea5ee909c67cacc93b86
jacklenzotti <jacklenzotti@gmail.com>
2020-06-21 15:36:18 -0400

    Updates

--- _next/static/8CCv4KJMKXLJU-zK5K48v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/posts/[id]":[a,b,c]}}("static\u002Fchunks\u002Fa9a7754c.355dffa44dd6de3c4d2f.js","static\u002Fchunks\u002Fbe6a5bc3e768aaef21dbfcfad34fef108112e606.98a169d0c7c69a2007b4.js","static\u002Fcss\u002F2f3d92715f384264fdcf.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/FQg1CTK-KEj2I7GZ1MqOT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/posts/[id]":[a,b,c]}}("static\u002Fchunks\u002Fa9a7754c.2c233710435a679a20bf.js","static\u002Fchunks\u002F48eb1bfe2ddfc3b167f5f462d5b2c1988feff564.6d43f9e16b432faf94bc.js","static\u002Fcss\u002F2f3d92715f384264fdcf.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
